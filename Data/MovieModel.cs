964606b28bfce7fa5e41b788a58f7dc8eced16a0
sugu-sano <sugu-sano@i-mobile.co.jp>
2019-11-20 17:01:07 +0900

    作った

---
+++ Data/MovieModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorMovieSearchApp.Data
+{
+    public class MovieModel
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Poster { get; set; }
+    }
+}
---
+++ Data/MovieRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorMovieSearchApp.Data
+{
+    public class MovieRepository
+    {
+        const string baseUrl = "https://www.omdbapi.com";
+
+        readonly IHttpClientFactory _factory;
+
+        public MovieRepository(IHttpClientFactory factory)
+            => _factory = factory;
+
+        public async ValueTask<MovieModel[]> ListAsync(string searchValue)
+        {
+            HttpClient client = _factory.CreateClient();
+            string json = await client.GetStringAsync(
+                $"{baseUrl}?s={searchValue}&apikey=4a3b711b");
+            Rootobject root = JsonSerializer.Deserialize<Rootobject>(json);
+
+            if (root.Response == "False")
+                throw new Exception(root.Error);
+
+            return root.Search;
+        }
+
+        class Rootobject
+        {
+            public MovieModel[] Search { get; set; }
+            public string Response { get; set; }
+            public string Error { get; set; }
+        }
+    }
+}
